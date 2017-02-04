ca68a9a992db999ddf5db5c5dc058e4d25424e82
DESKTOP-L3KVHB6\Dylan <dylan.vanbru@gmail.com>
2017-02-04 00:44:10 -0700

    BE32

--- BuildingEscape/Source/BuildingEscape/Grabber.cpp
+++ BuildingEscape/Source/BuildingEscape/Grabber.cpp
@@ -3,6 +3,7 @@
 #include "BuildingEscape.h"
 #include "Grabber.h"
 
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -10,8 +11,6 @@ UGrabber::UGrabber()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -19,17 +18,153 @@ UGrabber::UGrabber()
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!!"));
-	
+	FindPhysicsHandleComponent();
+	SetupInputComponent();
 }
 
-
 // Called every frame
-void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	///if physics handle is attatched
+	if (PhysicsComponent->GrabbedComponent)
+	{
+		GetPVPLAndRToLTDAndE();
+		///move object accordingly
+		PhysicsComponent->SetTargetLocation(LineTraceEnd);
+		FRotator ObjectRotation = ActorHit->GetActorRotation();
 
-	// ...
+		if (YawIsPressed)
+		{
+			PhysicsComponent->SetTargetRotation(ObjectRotation + FRotator(0, YawDirAndSpeed, 0));///form (pitch, yaw, roll)
+		}
+		if (PitchIsPressed)
+		{
+			PhysicsComponent->SetTargetRotation(ObjectRotation + FRotator(PitchDirAndSpeed, 0, 0));
+		}
+		if (RollIsPressed)
+		{
+			PhysicsComponent->SetTargetRotation(ObjectRotation + FRotator(0, 0, RollDirAndSpeed));
+		}
+	}
+}
+
+void UGrabber::FindPhysicsHandleComponent()
+{
+	PhysicsComponent = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+
+	if (PhysicsComponent) {}
+	else { UE_LOG(LogTemp, Error, TEXT("%s has no PhysicsComponent"), *GetOwner()->GetName()) }
+
+}
+
+void UGrabber::SetupInputComponent()
+{
+	MyInputComponent = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (MyInputComponent)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("%s has a InputComponent"), *GetOwner()->GetName())
+		///bind input axis
+		MyInputComponent->BindAction("Grab", IE_Pressed, this, &UGrabber::Grab);
+		MyInputComponent->BindAction("Grab", IE_Released, this, &UGrabber::Release);
+		MyInputComponent->BindAction("RotatePitch", IE_Pressed, this, &UGrabber::PitchPress);
+		MyInputComponent->BindAction("RotateYaw", IE_Pressed, this, &UGrabber::YawPress);
+		MyInputComponent->BindAction("RotateRoll", IE_Pressed, this, &UGrabber::RollPress);
+		MyInputComponent->BindAction("RotatePitch", IE_Released, this, &UGrabber::PitchRelease);
+		MyInputComponent->BindAction("RotateYaw", IE_Released, this, &UGrabber::YawRelease);
+		MyInputComponent->BindAction("RotateRoll", IE_Released, this, &UGrabber::RollRealease);
+	}
+	else { UE_LOG(LogTemp, Error, TEXT("%s is missing a InputComponent")) }
+}
+
+void UGrabber::Grab()
+{
+	auto HitResult = GetHitFromRayCast();
+	auto ComponentToGrab = HitResult.GetComponent();
+	ActorHit = HitResult.GetActor();
+
+	if (ActorHit)
+	{	///attatch physics handle
+		PhysicsComponent->GrabComponentAtLocationWithRotation(
+			ComponentToGrab, NAME_None, 
+			ComponentToGrab->GetOwner()->GetActorLocation(), 
+			ComponentToGrab->GetOwner()->GetActorRotation());
+	}
+}
+
+void UGrabber::Release()
+{
+	PhysicsComponent->ReleaseComponent();
+}
+
+void UGrabber::YawPress()
+{
+	YawIsPressed = true;
+}
+
+void UGrabber::PitchPress()
+{
+	PitchIsPressed = true;
+}
+
+void UGrabber::RollPress()
+{
+	RollIsPressed = true;
+}
+
+void UGrabber::YawRelease()
+{
+	YawIsPressed = false;
+}
+
+void UGrabber::PitchRelease()
+{
+	PitchIsPressed = false;
+}
+
+void UGrabber::RollRealease()
+{
+	RollIsPressed = false;
+}
+
+FHitResult UGrabber::GetHitFromRayCast()
+{
+	GetPVPLAndRToLTDAndE();
+	///set up guery params
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	///Line trace(AKA ray-cast) out to max grab distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+	return Hit;
+}
+
+void UGrabber::GetPVPLAndRToLTDAndE()
+{
+	//takes in the PVL and PVR parameters and writes its output to those vars
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, OUT PlayerViewPointRotation
+	);
+
+	//uses the above vars to calculate direction and endpoint
+	LineTraceDirection = PlayerViewPointRotation.Vector();
+	LineTraceEnd = PlayerViewPointLocation + (LineTraceDirection * Reach);
+
+	// draw red trace in the world to visualize
+	DrawDebugLine(GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(255, 0, 0),
+		false,
+		0.f,
+		0.f,
+		10.f
+	);
 }
 
--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -21,6 +21,53 @@ public:
 	// Called every frame
 	virtual void TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction ) override;
 
-		
-	
+private:
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	FVector LineTraceDirection;
+	FVector LineTraceEnd;
+	bool YawIsPressed = false;
+	bool PitchIsPressed = false;
+	bool RollIsPressed = false;
+	AActor *ActorHit;
+
+	UPhysicsHandleComponent *PhysicsComponent = nullptr;
+	UInputComponent *MyInputComponent = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	float Reach = 100.f;//how far in cm. in front of the player can they grab
+
+	UPROPERTY(EditAnywhere)
+	int32 YawDirAndSpeed = 4;
+
+	UPROPERTY(EditAnywhere)
+	int32 PitchDirAndSpeed = 4;
+
+	UPROPERTY(EditAnywhere)
+	int32 RollDirAndSpeed = 4;
+
+	//finds the (assumed) attatched physics handle component
+	void FindPhysicsHandleComponent();
+
+	//look for input component that appears at runtime
+	void SetupInputComponent();
+
+	//gets PlayerViewPointLocation And PlayerViewPointRotation plus calculates the LineTraceDirection And LineTraceEnd
+	void GetPVPLAndRToLTDAndE();
+
+	//does a raycast out to LineTraceEnd and returns what it hits if its a physics body
+	FHitResult GetHitFromRayCast();
+
+	//calls GetHitFromRaycast and determines what to do based on the hit
+	void Grab();
+
+	//TODO will drop the object the player is holding on key release
+	void Release();
+
+	void YawPress();
+	void PitchPress();
+	void RollPress();
+	void YawRelease();
+	void PitchRelease();
+	void RollRealease();
 };
